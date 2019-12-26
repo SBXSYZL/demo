f4bc8b3844566a7adc2a172d83c3fd7c7500751a
Your Name <you@example.com>
2019-12-27 00:31:26 +0800

    shipu

--- src/views/Foodrecipedetails.vue
+++ src/views/Foodrecipedetails.vue
@@ -13,15 +13,18 @@ export default {
       historyMsg:
 
           '<div>' +
-          '<div style="width: 20%;text-align: right"><img  style="width: 400px;height: 250px;float:left"' +
+          '<div style="width: 20%;text-align: right"><img  style="width: 400px;height: 300px;float:left"' +
           'src="http://site.meishij.net/r/208/102/1025708/s1025708_149663916013140.jpg">' +
           '</div>' +
-          '<h3 >' +
-          '&nbsp;&nbsp;&nbsp;&nbsp;很喜欢这道菜，简单的几种食材，因为有了排骨的加入，汤汁清甜滋润，口感超好呢，我喜欢在里面添加些颜色丰富的食材，这样五颜六色的一大锅，看着就有食欲。' +
-          '<h1>用料</h1>' +
-          '盐一朵' +
           '<br>' +
-          '胡萝卜一个' +
+          '&nbsp;&nbsp;&nbsp;&nbsp;很喜欢这道菜，简单的几种食材，因为有了排骨的加入，汤汁清甜滋润，口感超好呢，我喜欢在里面添加些颜色丰富的食材，这样五颜六色的一大锅，看着就有食欲。' +
+          '<br>' +
+          '<h1>用料</h1>' +
+          '<h3>主要用料</h3>' +
+          '<br>' +
+          '胡萝卜' +
+          '&nbsp;&nbsp;&nbsp;&nbsp;' +
+          '一个' +
           '<br>' +
           '玉米一个' +
           '<br>' +
@@ -33,14 +36,6 @@ export default {
           '<br>' +
           '<br>' +
           '<br>' +
-          '<br>' +
-          '<br>' +
-          '<br>' +
-          '<br>' +
-          '<br>' +
-          '<br>' +
-          '<br>' +
-
           '</h3>' +
           '</div>' +
           '<div style="display: flex">' +
@@ -50,39 +45,39 @@ export default {
           '<div>' +
           '<h4>1.排骨切成小段，冷水下锅汆烫，捞出备用</h4>' +
           '<br>' +
-          '<div style="width: 20%;text-align: right"><img style="width: 800px;height: 500px"' +
+          '<div style="width: 20%;text-align: right"><img style="width: 400px;height: 400px"' +
           'src="https://s1.st.meishij.net/rs/208/102/1025708/n1025708_149663931049182.jpg">' +
           '</div>' +
           '<div>' +
           '<h4>2.所有的食材都切成同样大小的块状</h4>' +
           '<br>' +
 
-          '<div style="width: 20%;text-align: right"><img style="width: 800px;height: 500px"' +
+          '<div style="width: 20%;text-align: right"><img style="width: 400px;height: 400px"' +
           'src="https://s1.st.meishij.net/rs/208/102/1025708/n1025708_149663931385949.jpg">' +
           '</div>' +
           '<h4>3.排骨倒入高压锅，加入清水</h4>' +
           '<br>' +
-          '<div style="width: 20%;text-align: right"><img style="width: 800px;height: 500px"' +
+          '<div style="width: 20%;text-align: right"><img style="width: 400px;height: 400px"' +
           'src="https://s1.st.meishij.net/rs/208/102/1025708/n1025708_149663931501397.jpg">' +
           '</div>' +
           '<h4>4.将排骨、玉米、山药、土豆都倒入高压锅，盖上盖子，煮熟</h4>' +
           '<br>' +
-          '<div style="width: 20%;text-align: right"><img style="width: 800px;height: 500px"' +
+          '<div style="width: 20%;text-align: right"><img style="width: 400px;height: 400px"' +
           'src="https://s1.st.meishij.net/rs/208/102/1025708/n1025708_149663931624768.jpg">' +
           '</div>' +
           '<h4>5.加入盐，拌匀</h4>' +
           '<br>' +
-          '<div style="width: 20%;text-align: right"><img style="width: 800px;height: 500px"' +
+          '<div style="width: 20%;text-align: right"><img style="width: 400px;height: 400px"' +
           'src="https://s1.st.meishij.net/rs/208/102/1025708/n1025708_149663931642655.jpg">' +
           '</div>' +
           '<h4>6.出锅装盘即可</h4>' +
           '<br>' +
-          '<div style="width: 20%;text-align: right"><img style="width: 800px;height: 500px"' +
+          '<div style="width: 20%;text-align: right"><img style="width: 400px;height: 400px"' +
           'src="https://s1.st.meishij.net/rs/208/102/1025708/n1025708_149663931777973.jpg">' +
           '</div>' +
            '<h2>玉米山药炖排骨成品图</h2>' +
 
-          '<div style="width: 20%;text-align: right"><img style="width: 800px;height: 600px"' +
+          '<div style="width: 20%;text-align: right"><img style="width: 400px;height: 400px"' +
           'src="https://s1.st.meishij.net/rs/208/102/1025708/n1025708_149664042920782.jpg">' +
           '</div>' +
           '</div>' +

177be83ebcdb1fc916a4f63128b228bf35c7883a
admin <836237106@qq.com>
2020-01-18 18:17:37 +0800

    用户主页更新

--- src/views/News/FoodNews.vue
+++ src/views/News/FoodNews.vue
@@ -5,14 +5,28 @@
         <h3>食品资讯</h3>
       </div>
       <div>
-        <el-button type="primary" icon="el-icon-edit" style="float: right" @click="writeIllustratedBook">添加资讯</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-edit"
+          style="float: right"
+          @click="writeIllustratedBook"
+          >添加资讯</el-button
+        >
       </div>
     </div>
 
     <div class="container">
       <div style="margin-bottom: 15px;">
-        <el-input placeholder="请输入内容" v-model="input" class="input-with-select">{{input.searchKey}}
-          <el-button slot="append" icon="el-icon-search" onclick="search"></el-button>
+        <el-input
+          placeholder="请输入内容"
+          v-model="input"
+          class="input-with-select"
+          >{{ input.searchKey }}
+          <el-button
+            slot="append"
+            icon="el-icon-search"
+            onclick="search"
+          ></el-button>
         </el-input>
       </div>
       <!--标签-->
@@ -22,17 +36,19 @@
           <el-row>
             <el-col :span="24" v-for="item in items" :key="item.id">
               <div>
-<!--                <div style=" float: left">-->
-<!--                  <img :src="item.img" class="image card-img">-->
-<!--                </div>-->
+                <!--                <div style=" float: left">-->
+                <!--                  <img :src="item.img" class="image card-img">-->
+                <!--                </div>-->
                 <!--中间-->
-                <div style="float: left" >
-                  <h3><span @click="itemClick(item.id)">{{item.title}}</span></h3>
+                <div style="float: left">
+                  <h3>
+                    <span @click="itemClick(item.id)">{{ item.title }}</span>
+                  </h3>
                   <div style="float:left;text-align: center">
-                    <p class="line-limit-length">{{item.newsDesc}}</p>
+                    <p class="line-limit-length">{{ item.newsDesc }}</p>
                   </div>
                   <div style="padding-top: 30px">
-                    <p style="align-content: center">{{item.releaseDate}}</p>
+                    <p style="align-content: center">{{ item.releaseDate }}</p>
                   </div>
                 </div>
 
@@ -42,15 +58,14 @@
                     @click.native.prevent="deleteRow(scope.$index, tableData4)"
                     type="danger"
                     size="mini"
-                  style="color: dodgerblue">
+                    style="color: dodgerblue"
+                  >
                     删除
                   </el-button>
                 </div>
               </div>
-
             </el-col>
           </el-row>
-
         </div>
         <!--分页-->
         <div class="block">
@@ -61,156 +76,154 @@
             :page-sizes="[100, 200, 300, 400]"
             :page-size="100"
             layout="total, sizes, prev, pager, next, jumper"
-            :total="400">
+            :total="400"
+          >
           </el-pagination>
         </div>
       </el-tabs>
-
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'FoodNews',
-    data () {
-      return {
-        items: [],
-        activeName2:'',
-        currentPage4:1,
-        pageNo:1,
-        pageSize:10,
-        input:{
-          pageNo:1,
-          pageSize:10,
-          searchKey:1
-       }
+export default {
+  name: 'FoodNews',
+  data () {
+    return {
+      items: [],
+      activeName2: '',
+      currentPage4: 1,
+      pageNo: 1,
+      pageSize: 10,
+      input: {
+        pageNo: 1,
+        pageSize: 10,
+        searchKey: 1
       }
+    }
+  },
+  methods: {
+    itemClick (key) {
+      this.$router.push({
+        path: '/newsDetails',
+        query: {
+          id: key
+        }
+      })
     },
-    methods: {
-      itemClick(key) {
-        this.$router.push({
-          path: '/newsDetails',
-          query: {
-            id: key
-          }
-        })
-      },
-      handleSizeChange(val) {
-        console.log(`每页 ${val} 条`)
-      },
-      handleCurrentChange(val) {
-        console.log(`当前页: ${val}`)
-      },
-      writeIllustratedBook() {
-        this.$router.push('/write')
-      },
-      detail() {
-        this.$router.push('/newsdetails')
-
-      },
-      search(){
-        this.$router.push('')
-      },
-      handleClick() {
-      },
-      deleteRow(val, data) {
-      },
-
-      getNewsList() {
-        this.$axios.get('/api/user/getNewsList', {
-          params: {
-            pageNo: 1,
-            pageSize: 10
-          }
-        }).then(res => {
-          console.log(res)
-          this.items = res.data.data.list;
-        }).catch(err => {
-          console.log(err)
-        })
-      },
-      getSearchNews() {
-        this.$axios.get('/api/user/searchNews', {
-          params: {
-            pageNo: 1,
-            pageSize: 10,
-            searchKey: 1
-          }
-        }).then(res => {
-          console.log(res)
-          this.input = res.data.data.list;
-        }).catch(err => {
-          console.log(err)
-        })
-      },
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
     },
-    created() {
-      this.getNewsList();
-      this.getSearchNews();
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+    },
+    writeIllustratedBook () {
+      this.$router.push('/write')
+    },
+    detail () {
+      this.$router.push('/newsdetails')
+
+    },
+    search () {
+      this.$router.push('')
+    },
+    handleClick () {
+    },
+    deleteRow (val, data) {
     },
 
-  }
+    getNewsList () {
+      this.$axios.get('/api/user/getNewsList', {
+        params: {
+          pageNo: 1,
+          pageSize: 10
+        }
+      }).then(res => {
+        console.log(res)
+        this.items = res.data.data.list;
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    getSearchNews () {
+      this.$axios.get('/api/user/searchNews', {
+        params: {
+          pageNo: 1,
+          pageSize: 10,
+          searchKey: 1
+        }
+      }).then(res => {
+        console.log(res)
+        this.input = res.data.data.list;
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+  },
+  created () {
+    this.getNewsList();
+    this.getSearchNews();
+  },
+
+}
 
 </script>
 
 <style scoped>
-  .el-row {
-    margin-bottom: 30px;
-    height: 5%;
-  }
+.el-row {
+  margin-bottom: 30px;
+  height: 5%;
+}
 
-  .el-col {
-    border-radius: 14px;
-  }
+.el-col {
+  border-radius: 14px;
+}
 
-  .card-img {
-    width: 200px;
-    height: 200px;
-  }
+.card-img {
+  width: 200px;
+  height: 200px;
+}
 
-  . image {
-    width: auto;
-    height: 50px;
-  }
-  .time {
-    font-size: 13px;
-    color: #999;
-  }
+.image {
+  width: auto;
+  height: 50px;
+}
+.time {
+  font-size: 13px;
+  color: #999;
+}
 
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
 
-  .button {
-    padding: 0;
-    float: right;
-  }
+.button {
+  padding: 0;
+  float: right;
+}
 
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
 
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
+.clearfix:after {
+  clear: both;
+}
 
-  .clearfix:after {
-    clear: both
-  }
+.el-select .el-input {
+  width: 130px;
+}
 
-  .el-select .el-input {
-    width: 130px;
-  }
+.input-with-select .el-input-group__prepend {
+  background-color: #fff;
+}
 
-  .input-with-select .el-input-group__prepend {
-    background-color: #fff;
-  }
-
-  .line-limit-length {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-
+.line-limit-length {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
 </style>

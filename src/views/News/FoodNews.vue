9c8aae13933706afcf49995c7ae657d1d18c2530
CharonL <1603360292@qq.com>
2020-01-18 16:11:49 +0800

    pyh:删除新闻

--- src/views/News/FoodNews.vue
+++ src/views/News/FoodNews.vue
@@ -11,8 +11,8 @@
 
     <div class="container">
       <div style="margin-bottom: 15px;">
-        <el-input placeholder="请输入内容" v-model="input" class="input-with-select">{{input.searchKey}}
-          <el-button slot="append" icon="el-icon-search" @click="searchNews"/>
+        <el-input placeholder="请输入内容" v-model="searchKey" class="input-with-select">
+          <el-button slot="append" icon="el-icon-search" @click="getSearchNews"/>
         </el-input>
       </div>
       <!--标签-->
@@ -20,7 +20,7 @@
         <el-table
           :data="items"
           style="width: 100%"
-          @row-click="itemClick"
+          @row-click.stop="itemClick"
         >
           <el-table-column style="width: 80%;position: absolute;">
             <template slot-scope="scope" >
@@ -54,7 +54,7 @@
           :before-close="handleClose">
               <span slot="footer" class="dialog-footer" v-model="title">
               <el-button @click="dialogVisible = false">取 消</el-button>
-              <el-button type="primary" @click="deleteNewsConfirm">确 定</el-button>
+              <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
                </span>
         </el-dialog>
 
@@ -83,8 +83,8 @@
         items: [],
         pageNo:1,
         pageSize:10,
-        input:''
-
+        searchKey: '',
+        dialogVisible: false,
       }
     },
     methods: {
@@ -116,54 +116,10 @@
       },
       deleteNews(row) {
         this.dialogVisible = true;
-        if(window.confirm("是否确定删除？")) {
-          this.items.splice(row, 1)
-        }
+        this.items.splice(row, 1)
       },
-      deleteNewsConfirm() {
-        console.log(123)
-        if (this.title != null ) {
-          this.$confirm('确定上删除当前内容？', '验证', {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
-            type: 'info'
-          }).then(() => {
-            let params = new URLSearchParams();
-            params.append("title", this.inputTitle);
-            params.append("newsDesc", this.inputDesc);
-            params.append("content", this.content)
-            this.$axios({
-              method: 'post',
-              url: '/api/admin/FoodNews',
-              data: params
-            }).then(res => {
-              console.log(res);
-              if (res.data.status === 'success' && res.data.data === 'success') {
-                this.$message({
-                  type: 'success',
-                  message: '上传成功!'
-                });
-                this.$router.push('/FoodNews')
-              } else {
-                this.$message.error(res.data.data.errMsg);
-              }
-            }).catch(err => {
-              this.$message.error('不知道啥原因上传失败...');
-            })
-
-          }).catch(() => {
-            this.$message({
-              type: 'info',
-              message: '已取消上传'
-            })
-          });
-          this.dialogVisible = false;
-        }
-      },
-
 
       getNewsList() {
-
         this.$axios.get('/api/user/getNewsList', {
           params: {
             pageNo: this.pageNo,
@@ -176,43 +132,30 @@
           console.log(err)
         })
       },
-      getSearchNews() {
-        this.$axios.get('/api/user/searchNews', {
+      getSearchNews(){
+        this.pageNo = 1;
+        this.$axios.get('/api/admin/searchNews', {
           params: {
             pageNo: this.pageNo,
             pageSize: this.pageSize,
-            searchKey: this.input
+            searchKey: this.searchKey
           }
         }).then(res => {
-          console.log(res)
-          this.input = res.data.data.list;
-        }).catch(err => {
-          console.log(err)
-        })
-      },
-      getDeleteNews(){
-        this.$axios.get('/api/user/deleteNews',{
-          "params":{
-            "newsId":1
-          }
-        }).catch(err => {
-          console.log(err)
-        })
-      },
-      getWriteNews(){
-        this.$axios.get('/api/user/WriteNews',{
-          params:{
-            content:'adadad',
-            newsDesc:'ada',
-            title:a,
-          }
-        }).then(res => {
+          console.log("res:")
           console.log(res)
           this.items = res.data.data.list;
         }).catch(err => {
           console.log(err)
         })
-      }
+      },
+      handleSizeChange(val) {
+        this.pageSize = val;
+        this.getNewsList();
+      },
+      handleCurrentChange(val) {
+        this.pageNo=val;
+        this.getNewsList();
+      },
     },
     created() {
       // this.$router.go(0);

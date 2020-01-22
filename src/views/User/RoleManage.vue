89327bba8de2b19cc22b073a77391f6e4c48159e
SBXSYZL <2245440426@qq.com>
2020-01-22 22:14:18 +0800

    错误

--- src/views/User/RoleManage.vue
+++ src/views/User/RoleManage.vue
@@ -80,7 +80,7 @@ export default {
     }
   },
   mounted () {
-    this.$axios.get('http://localhost:8088/admin/getUserList', {
+    this.$axios.get('/api/admin/getUserList', {
       params: {
         "pageNo": this.pageNo,
         "pageSize": this.pageSize
